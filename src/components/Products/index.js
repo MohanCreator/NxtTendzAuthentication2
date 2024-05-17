4c319325bd113f85b2a609fe346060bf48764e78
Mohan <nxtwave@ip-10-2-78-220.ap-south-1.compute.internal>
2024-05-17 07:20:04 +0000

    AuthenticatAndUnAuthenticate

---
+++ src/components/Products/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const Products = () => (
+  <div className="product-container">
+    <img
+      className="image"
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+    />
+  </div>
+)
+export default Products
